9a078eb22e2804be36c0ea02ec40890ebb01a6a2
Jeremy Felt <jeremy.felt@gmail.com>
2012-02-24 21:10:27 -0800

    First refactor of index

--- index.php
+++ index.php
@@ -1,46 +1,27 @@
 <?php
 
-define('MY_IP', '24.22.85.180' );
+define( 'MY_IP', '24.22.85.180' );
 
-/*  Because of the authorization with Twitter, we want to use
-    the same domain name for every request when logging in.
-*/
+/* Because of the authorization with Twitter, we want to use
+ * the same domain name for every request when logging in. */
+if ( 'defollower.com' != $_SERVER[ 'HTTP_HOST' ] ) != "defollower.com")
+	header( 'Location: http://defollower.com' );
 
-if ($_SERVER['HTTP_HOST'] != "defollower.com"){
+require_once( 'config.php' );
+require_once( 'includes/twitteroauth.php' );
 
-    header("Location:http://defollower.com");
-    exit();
-
-}
-
-require_once('config.php');
-require_once('includes/twitteroauth.php');
-
-/*  Temporary, to block outside use. */
-
-if($_SERVER['REMOTE_ADDR'] != MY_IP){
-
-    include 'views/placeholder.php';
-    exit;
-
+if( $_SERVER['REMOTE_ADDR'] != MY_IP ){
+	include 'views/placeholder.php';
+	exit;
 }else{
+	/* Start the session and check if we already have an access token.
+	 * If we don't, display the login page. */
+	session_start();
+	if ( isset( $_SESSION[ 'access_token' ] ) ){
+		include 'controllers/display_stale.php';
+		exit();
+	}
+	session_write_close();
 
-    /*  Start the session and check if we already have an access token.
-        If we don't, display the login page.
-    */
-
-    session_start();
-    if (isset($_SESSION['access_token'])){
-
-        include 'controllers/display_stale.php';
-        exit();
-
-    }
-
-    session_write_close();
-
-    include 'views/login.php';
-
+	include 'views/login.php';
 }
-
-?>
