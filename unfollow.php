18e12ab47c23b47cf83cfad6491ac977d8c9f3ad
Jeremy Felt <jeremy.felt@gmail.com>
2012-02-24 21:14:32 -0800

    First refactor of unfollow

--- unfollow.php
+++ unfollow.php
@@ -1,24 +1,22 @@
 <?php
 session_start();
-require_once('includes/twitteroauth.php');
-require_once('config.php');
+require_once( 'config.php' );
+require_once( 'includes/twitteroauth.php' );
 
 /* If access tokens are not available redirect to connect page. */
-if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
-    header('Location: ./clearsessions.php');
-}
+if ( empty( $_SESSION[ 'access_token' ] ) || empty( $_SESSION[ 'access_token' ][ 'oauth_token' ] ) || empty( $_SESSION[ 'access_token' ][ 'oauth_token_secret' ] ) )
+    header( 'Location: ./clearsessions.php' );
+
 /* Get user access tokens out of the session. */
-$access_token = $_SESSION['access_token'];
-$friend_id = $_GET['fid'];
+$access_token = $_SESSION[ 'access_token' ];
+$friend_id = $_GET[ 'fid' ];
 
 /* Create a TwitterOauth object with consumer/user tokens. */
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+$connection = new TwitterOAuth( CONSUMER_KEY, CONSUMER_SECRET, $access_token[ 'oauth_token' ], $access_token[ 'oauth_token_secret' ] );
+$oauth_options = array( 'user_id' => $friend_id );
+$unfollow_result = $connection->post( 'friendships/destroy', $oauth_options );
 
-$oauth_options = array('user_id' => $friend_id);
-
-$unfollow_result = $connection->post('friendships/destroy', $oauth_options);
-
-var_dump($unfollow_result);
+var_dump( $unfollow_result );
 
 ?>
 <br><br>
