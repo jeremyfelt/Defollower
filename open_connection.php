b39dc28b22e59945fcc50a6ed600fad7a801342b
jeremyfelt <jeremy.felt@gmail.com>
2012-10-20 14:46:39 -0700

    Code cleanup, slight rework of the open connection script. Next step will be to process the callback from Twitter properly. We'll use SESSION for now, but try to ditch that soon.

--- open_connection.php
+++ open_connection.php
@@ -8,23 +8,21 @@ session_start();
 require_once( 'includes/twitteroauth.php' );
 require_once( 'config.php' );
 
-/* Build TwitterOAuth object with client credentials. */
+// Build a new TwitterOAuth object with client credentials from our config
 $connection = new TwitterOAuth( CONSUMER_KEY, CONSUMER_SECRET );
-/* Get temporary credentials. */
 $request_token = $connection->getRequestToken( OAUTH_CALLBACK );
 
-/* Save temporary credentials to session. */
-$_SESSION[ 'oauth_token' ] = $token = $request_token[ 'oauth_token' ];
-$_SESSION[ 'oauth_token_secret' ] = $request_token[ 'oauth_token_secret' ];
+// Save temporary credentials to the session, though I'd rather use cookies or something for this
+// @todo ditch the session stuff
+$_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 
-/* If the last connection failed, don't display authorization link. */
-switch ( $connection->http_code ) {
-	case 200:
-		/* Build authorize URL and redirect user to Twitter. */
-		$url = $connection->getAuthorizeURL( $token );
-		header( 'Location: ' . $url );
-		break;
-	default:
-		/* Show notification if something went wrong. */
-		echo 'Could not connect to Twitter. Refresh the page or try again later.';
+if ( 200 == $connection->http_code ) {
+	header( 'Location: ' . $connection->getAuthorizeURL( $token ) );
+	die();
 }
+
+include 'views/header_preauth.php';
+// Something didn't work right.
+echo 'Could not connect to Twitter. Refresh the page or try again later';
+include 'views/footer_preauth.php';
--- views/footer_preauth.php
+++ views/footer_preauth.php
@@ -1,6 +1,4 @@
-<?php
-?>
-<!-- Piwik --> 
+<!-- Piwik -->
 <script type="text/javascript">
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.foghlaim.com/" : "http://stats.foghlaim.com/");
 document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
