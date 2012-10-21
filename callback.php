28c65ddf350b15a0e79c4bf61086ebc73b158bd8
jeremyfelt <jeremy.felt@gmail.com>
2012-10-20 18:58:48 -0700

    Remove all DB stuff, totally not necessary.

--- callback.php
+++ callback.php
@@ -7,7 +7,6 @@
 session_start();
 require_once('includes/twitteroauth.php');
 require_once('config.php');
-//require_once('includes/database.php');
 
 /* If the oauth_token is old redirect to the connect page. */
 if ( isset( $_REQUEST['oauth_token'] ) && $_SESSION['oauth_token'] !== $_REQUEST['oauth_token'] ) {
@@ -24,41 +23,6 @@ $access_token = $connection->getAccessToken( $_REQUEST['oauth_verifier'] );
 /*  These two variables need to be stored. */
 $access_token['oauth_token'];
 $access_token['oauth_token_secret'];
-
-/*
-$user_id = NULL;
-
-//$query_existing_token = $db->prepare("SELECT user_id
-//    FROM users
-//    WHERE oauth_token=? AND oauth_token_secret=?");
-
-//$query_existing_token->bind_param('ss',$access_token['oauth_token'], $access_token['oauth_token_secret']);
-//$query_existing_token->execute();
-//$query_existing_token->bind_result($user_id);
-//$query_existing_token->fetch();
-//$query_existing_token->close();
-
-if (!$user_id){
-
-    $last_login_date = date('Y-m-d H:i:s');
-
-    $query_add_token = $db->prepare("INSERT INTO users (oauth_token, oauth_token_secret, last_login_date)
-        VALUES (?,?,?)");
-
-    $query_add_token->bind_param('sss', $access_token['oauth_token'], $access_token['oauth_token_secret'], $last_login_date);
-    $query_add_token->execute();
-    
-    $user_id = $db->insert_id;
-
-    $query_add_token->close();
-
-}
-*/
-//$_SESSION['user_id'] = $user_id;
-$_SESSION['access_token'] = $access_token;
-
-//$db->close();
-
 
 /* Remove no longer needed request tokens */
 unset($_SESSION['oauth_token']);
--- includes/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_user = "defollower";
-$db_pass = "9e!cRD4t";
-$db_name = "defollower";
-$db_host = "127.0.0.1";
-
-$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
