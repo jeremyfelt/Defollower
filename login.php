4cafaf717208e64e29d999d0d290ebf639e16134
jeremyfelt <jeremy.felt@gmail.com>
2012-10-21 13:47:00 -0700

    Add default text to the login screen too so that yeah.

--- login.php
+++ login.php
@@ -1,5 +1,9 @@
 <div class="announce">
 	<h1>This Is Defollower</h1>
-	<br>
+	<p>And this is the list of the tweeps you follow that have <b>not</b> said anything in at least 30 days.
+		Why does that matter? To some it may not. To others, Twitter is an peer based ecosystem in which we rely on
+		our relationships to lead us to new ones. Recommendation engines may peer into our following lists and use
+		that data to recommend that others follow the same users that we are already following. Imagine the confusion
+		when a recommended followee appears that hasn't uttered a character, let alone 140, in months.</p>
 	<a href="./open_connection.php"><img src="./images/sign-in-with-twitter-gray.png" alt="Sign in with Twitter"/></a>
 </div>
